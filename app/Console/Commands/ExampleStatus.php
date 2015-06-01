a51ab80d66e29421ce5b41e5b610c22e0d46f308
Stephen Rees-Carter <stephen@rees-carter.net>
2015-06-01 21:06:26 +1000

    Adding missing command.

---
+++ app/Console/Commands/ExampleStatus.php
@@ -0,0 +1,46 @@
+<?php namespace App\Console\Commands;
+
+use App\Status;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class ExampleStatus extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'example:status';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Updates the status example record.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		$status = new Status;
+		$status->key = 'cron';
+		$status->value = true;
+		$status->save();
+	}
+}
